1edb813e3fd1d3b17bdd637b48b79ac10d6e4b5d
anugrahsivadasan-react <anugrahshivadasan@ziyaacademy.co.in>
2026-04-18 12:39:53 +0530

    changed contents fot seo optimisation

---
+++ src/components/SEO.tsx
@@ -0,0 +1,30 @@
+import { Helmet } from "react-helmet-async";
+
+type SEOProps = {
+  title: string;
+  description: string;
+  keywords: string;
+};
+
+const SEO = ({ title, description, keywords }: SEOProps) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+
+      {/* Open Graph (for social sharing) */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+    </Helmet>
+  );
+};
+
+export default SEO;
